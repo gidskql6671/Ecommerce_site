6a073b8249f65c9b26bc17cf2e530349fdf88015
김동환 <gidskql6671@gmail.com>
2020-11-07 07:06:33 +0000

    DB 연결

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 let router = require('./routes/index')(app);
 
-let port = 3000;
+dotenv.config();
+
+let port = process.env.PORT || 3000;
+let url = process.env.MONGO_URL;
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
@@ -11,3 +16,14 @@ let server = app.listen(port, function(){
 	console.log(`Server has started on port ${port}`);
 })
 
+mongoose.connect(url,
+				{useNewUrlParser : true,
+				useUnifiedTopology: true});
+db = mongoose.connection;
+
+db.on('error', function(err){
+	console.log(`DB Error : ${err}`);
+})
+db.on('open', function(){
+	console.log('DB Open');
+})
